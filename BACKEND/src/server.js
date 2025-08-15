241357740c2a79d71a9f39a72298f8f6cea388f9
veera-2005 <124985199+veera-2005@users.noreply.github.com>
2025-08-15 21:41:59 +0530

    prepared for the deployment

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import routerNotes from "./Router/routesNotes.js";
 import { connectDB } from "./Config/db.js";
@@ -9,18 +10,26 @@ import rateLimitMiddleware from "./Middleware/ratelimit.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); // Ensure __dirname is defined correctly
 
 // Middleware it will allow to access the request body
 const app = express(); 
-
-app.use(cors( // Corrected the typo from 'cros' to 'cors'
-    {origin: "http://localhost:5173",} // Adjust this to your frontend URL
-));
+if(process.env.NODE_ENV !== "production"){
+        app.use(cors( // Corrected the typo from 'cros' to 'cors'
+            {origin: "http://localhost:5173",} // Adjust this to your frontend URL
+        ));
+}
 
 app.use(express.json());
 app.use(rateLimitMiddleware);
 app.use("/api/notes", routerNotes);
-
+        if(process.env.NODE_ENV === "production") {
+            
+        app.use(express.static(path.join(__dirname, "../FRONTEND/dist"))); // Corrected 'joid' to 'join'
+        app.get("*" , (req, res) => {
+            res.sendFile(path.join(__dirname, "../FRONTEND/dist/index.html")); // Corrected 'joid' to 'join'
+        });
+    }
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server started on port:", PORT);
