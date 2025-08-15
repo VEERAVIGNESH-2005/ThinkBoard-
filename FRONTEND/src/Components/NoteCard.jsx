4ef334ab2f43d85babc459ea3a8b954f1cb41e26
VEERAVIGNESH-2005 <veeravigneshjayakumar@gmail.com>
2025-08-15 22:00:33 +0530

    Update NoteCard.jsx

--- FRONTEND/src/Components/NoteCard.jsx
+++ FRONTEND/src/Components/NoteCard.jsx
@@ -37,7 +37,9 @@ const NoteCard = ({ note , setNotes }) => {
                 {formatDate(new Date(note.createdAt))}  
             </span>
             <div className="flex items-center gap-1">
-              <PenSquareIcon className="size-4" />
+               <button className="btn btn-ghost btn-xs text-error">
+                  <PenSquareIcon className="size-4" />
+                </button>
               <button className="btn btn-ghost btn-xs text-error" onClick={(e) => handleDelete(e, note._id)}>
                 <Trash2Icon className="size-4" />
               </button>
